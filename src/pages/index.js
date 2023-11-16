96605612edb97310f7199dd3f0993bb0348c1034
Kim Nguyen <kkn2011@nyu.edu>
2023-11-16 08:55:35 -0500

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
-export default function Home() {
-    return (
-        <div>
-            <h1>Exercise Six</h1>
-        </div>
+import Header from "@/app/components/Header"
 
+export default function UserProfile() {
+    return (
+    <>
+        <Header /> 
+        <main>
+            <h1>User Profile</h1>
+        </main>
+
+        </>
 
     )
 } 

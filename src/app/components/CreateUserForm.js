cce64887e05e7658297a49c2c2fd79d7723ec866
Kim Nguyen <kkn2011@nyu.edu>
2023-11-25 20:48:25 -0500

    form

--- src/app/components/CreateUserForm.js
+++ src/app/components/CreateUserForm.js
@@ -1,8 +1,22 @@
+import styles from "./components.module.css"; 
+
 
 const CreateUser = () => {
     return (
         <div>
             <h2>Create User</h2>
+            <form className={styles.form}>
+                <label htmlFor= "name">Name</label>
+                <input type="text" name="name" />
+
+                <label htmlFor= "email">Email</label>
+                <input type="email" name="email" />
+
+                <label htmlFor= "pass">Password</label>
+                <input type="password" name="pass" />
+
+                <button type= "submit">Create User</button>            
+            </form>
         </div>
     )
 }
--- src/app/components/LoginForm.js
+++ src/app/components/LoginForm.js
@@ -1,8 +1,19 @@
+import styles from "./components.module.css"; 
+
 
 const LoginForm = () => {
     return (
         <div>
             <h2>Login Form</h2>
+            <form className={styles.form}>
+                <label htmlFor= "email">Email</label>
+                <input type="email" name="email" />
+
+                <label htmlFor= "pass">Password</label>
+                <input type="password" name="pass" />
+
+                <button type= "submit">Create User</button>            
+            </form>
         </div>
     )
 }

9afb9f8bc65cf63db19f08a7137dab60ee8c79a4
Kim Nguyen <kkn2011@nyu.edu>
2023-11-30 09:41:05 -0500

    fixed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,65 @@
+import { useCallback, useEffect, useState } from "react"; 
+import { initializeApp } from "firebase/app"; 
+import { getAuth, onAuthStateChanged } from "firebase/auth"; 
 import Header from "@/app/components/Header"; 
+import firebaseConfig from "@/app/components/firebaseConfig";
 
 export default function MyApp( { Component, pageProps }) {
+    const [appIntialized, setAppIntialized] = useState(false); 
+    const [isLoading, setIsLoading] = useState(true); 
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [userInformation, setUserInformation] = useState(null); 
+
+    const createUser = useCallback((e) => {
+
+    }, []); 
+
+    const loginUser = useCallback((e) => {
+
+    }, [] ); 
+
+    const logoutUser = useCallback(() => {
+
+    }, [] ); 
+
+    useEffect(() => {
+        initializeApp(firebaseConfig); 
+        setAppIntialized(true); 
+    }, []); 
+
+    useEffect(() => {
+        if (appIntialized) {
+            const auth = getAuth(); 
+            onAuthStateChanged(auth, (user) => {
+                if (user) {
+
+                    setUserInformation(user); 
+                    setIsLoggedIn(true); 
+                } else {
+                    setUserInformation(null); 
+                    setIsLoggedIn(false); 
+                }
+
+                setIsLoading(false); 
+
+            }); 
+        }
+
+    }, [appIntialized]); 
+
+    if (isLoading) return null; 
+
 
     return (
         <>
             <Header />
             <Component 
             {...pageProps}
+            createUser={createUser}
+            logoutUser={logoutUser}
+            loginUser={loginUser}
+            isLoggedIn={isLoggedIn}
+            userInformation={userInformation}
     
         />
         </>
